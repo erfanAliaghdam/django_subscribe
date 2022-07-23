baa5f6ac35da6865c491dcb6c7dfe61d495a7ff8
erfanAliaghdam <80113382+erfanAliaghdam@users.noreply.github.com>
2022-07-23 16:07:01 +0430

    customized User Model

--- core/admin.py
+++ core/admin.py
@@ -4,16 +4,15 @@ from django.contrib.auth.admin import UserAdmin as AdminUser
 
 @admin.register(get_user_model())
 
-class UserAdmin(AdminUser):
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'is_staff', 'is_active', 'is_superuser')
     search_fields = ('email', 'first_name', 'last_name')
-    list_display = ('email', 'first_name', 'last_name', 'is_staff')
-    add_fieldsets = (
-        (
-            None,
-            {
-                "classes": ("wide",),
-                "fields": ("email", "password1", "password2"),
-            },
-        ),
-    )
     ordering = ('email',)
+    add_fieldsets = (
+    (None, {
+        'classes': ('wide',),
+        'fields': ('email', 'first_name', 'last_name', 'password1', 'password2'),
+    }),
+    )
+    
+    list_filter = ('is_staff', 'is_superuser', 'is_active',)
--- project/settings.py
+++ project/settings.py
@@ -153,3 +153,11 @@ SIMPLE_JWT = {
 }
 #! --------USER----------
 AUTH_USER_MODEL = 'core.User'
+
+
+#!---------CELERY----------
+BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
